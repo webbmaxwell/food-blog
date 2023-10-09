e9c54de5b829b84f8ba60b0642d92daca8607116
webbmaxwell <webbwrites@gmail.com>
2023-10-09 09:46:08 -0600

    Adding french onion soup blogpost

---
+++ src/Components/BlogPosts/FrenchOnionSoup.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+function FrenchOnionSoup() {
+    return(
+        <div>
+            <h3>Exploring French Onion Soup</h3>
+            <p>I have been a fan of French Onion Soup since I first tried it around 5 years ago on a date to the Red Rock Brewery with my wife. I had never experienced flavors in that combination before, or that much cheese in a single dish!
+            I have been meaning to try it myself, but between the time it takes to make it, needing the ramekins/oven-safe bowls, and the cost of the dang Gouda, I had plenty of reasons to keep delaying my attempt. That all changed when I discovered *this recipe* on Pinterest for a slow-cooker French Onion Soup!
+            Expenses justified, it was still pretty time-consuming; I had to start cooking the onions at 10 PM Saturday night, leave them on until 8 AM when I got up to add the broth, then let that cook as a whole soup until around 4 PM. Then I served up the soup into my ramekins and added the cheese and bread, baked those to melt the cheese, then broiled for a couple of minutes to brown the cheese.
+            And it was fantastic.
+            I will say, it seems like I let the cheese broil a bit too long because it ended up a little bit too melted, but all in all it turned out great. I paired it with breadsticks and olive oil/balsamic vinegar, and a lemon arugula salad that included a homemade lemon vinaigrette. I was pretty proud of how it all came together (my mother-in-law even asked me for the recipe!).
+            Needless to say, I will be trying this recipe again</p>
+        </div>
+    )
+}
+
+export default FrenchOnionSoup;
