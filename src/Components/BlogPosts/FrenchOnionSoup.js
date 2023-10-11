b2aa8bcb584940719b1968dc20a6f3acd10e41c7
webbmaxwell <webbwrites@gmail.com>
2023-10-10 18:08:53 -0600

    Fixing the pun in the French Onion title

--- src/Components/BlogPosts/FrenchOnionSoup.js
+++ src/Components/BlogPosts/FrenchOnionSoup.js
@@ -5,7 +5,7 @@ function FrenchOnionSoup() {
     let year = now.getFullYear();
     return(
         <div className="Blogpost" id="latest">
-            <h3>French Onion Superpower</h3>
+            <h3>French Onion (Soup)-erpower</h3>
             <p>I have been a fan of French Onion Soup since I first tried it around {year - 2018} years ago on a date to the Red Rock Brewery with my wife. I had never experienced flavors in that combination before, or that much cheese in a single dish!</p>
             <p>I have been meaning to try it myself, but between the time it takes to make it, needing the ramekins/oven-safe bowls, and the cost of the dang Gouda, I had plenty of reasons to keep delaying my attempt. That all changed when I discovered <a href="https://www.thekitchn.com/how-to-make-french-onion-soup-in-the-slow-cooker-start-to-finish-cooking-lessons-from-the-kitchn-215696?utm_source=pinterest&utm_medium=social&utm_campaign=managed&utm_content=custompin&crlt.pid=camp.1fPOaGLAuX01" target="_blank">this recipe</a> on Pinterest for a slow-cooker French Onion Soup!</p>
             <p>Expenses justified, it was still pretty time-consuming; I had to start cooking the onions at 10 PM Saturday night, leave them on until 8 AM when I got up to add the broth, then let that cook as a whole soup until around 4 PM. Then I served up the soup into my ramekins and added the cheese and bread, baked those to melt the cheese, then broiled for a couple of minutes to brown the cheese.</p>
