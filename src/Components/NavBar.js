e324220ddc14f679d920a062ae12471899123538
webbmaxwell <webbwrites@gmail.com>
2023-11-24 16:11:06 -0700

    adding 'Recipes' line to navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,6 +6,7 @@ export default function NavBar() {
             <ul>
                 <li>Home</li>
                 <li>All Posts</li>
+                <li>Recipes</li>
                 <li>About</li>
             </ul>
         </nav>
