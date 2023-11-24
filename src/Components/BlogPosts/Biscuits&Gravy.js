2d84b1e604daf22a892bb00aac7330630ab1bf33
webbmaxwell <webbwrites@gmail.com>
2023-11-24 13:58:29 -0700

    Biscuits&Gravy article written

---
+++ src/Components/BlogPosts/Biscuits&Gravy.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+function BiscuitsNGravy() {
+    return(
+        <div>
+            <h3>Biscuits & Gravy</h3>
+            <p>Growing up, I had heard about biscuits and gravy, and I have always been a fan of biscuits, but never tried the dish. That was until I was in high school and helping a friend move. </p>    
+            <p>We got up early on a Saturday and loaded his stuff into my car to take down to his new house that was just down the road.</p>
+            <p>I remember laughing because he hadn't really packed anything into boxes, they were mostly just piles on his bed,so we heaved armfulls of clothes and packed his desk into my little Toyota Camry and drove down the hill to his mom's new place.</p>
+            <p>Once we had transferred his wardrobe from one bed to another, we headed back up to the old house to help his mom finish up, and she had a huge pile of biscuits and gravy waiting for us. We were starving after moving all morning, and it hit the spot perfectly.</p>
+            <p>I have been craving it again recently, so when I woke up on Friday and saw that we had some leftover Thanksgiving gravy, I knew my time had come. </p>
+            <p>I popped open a packet of Pillsbury Flaky Biscuits and threw them in the air frier (blasphemy? maybe, but it friggin works). I didn't have enough gravy to cover all the sausage we were going to use, so I had to whip up a little of my own. I used some bacon grease we had saved and fried up the turkey sausage we had (another Thanksgiving flavor!), then threw on some flour, milk, salt and pepper, along with the leftover gravy.</p>
+            <p>Ideal consistency achieved, I ladled some on over my air-fried biscuits and voila! It actually turned out really well. Both my wife and my sister-in-law attest that they are not typically fans of biscuits and gravy, but they both cleaned their plates!</p>
+        </div>
+    )
+};
+
+export default BiscuitsNGravy;
